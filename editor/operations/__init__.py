f7d9c0193d9fde31f3b4f7ceb7aeedda02aedaa3
Andras Schmelczer <andras@schmelczer.dev>
2024-04-01 12:48:13 +0100

    Add editing methods

---
+++ editor/operations/__init__.py
@@ -0,0 +1,3 @@
+from .add_noise import add_noise
+from .change_temperature import change_temperature
+from .add_random_colour_spill import add_random_colour_spill
---
+++ editor/utils/__init__.py
@@ -0,0 +1,4 @@
+from .interpolate import interpolate
+from .random import random
+from .apply_pixel_shader import apply_pixel_shader
+from .get_colour_lut import get_colour_lut
