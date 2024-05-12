3974894d60cd1d461384355005b1be4b4df8f47d
Andras Schmelczer <andras@schmelczer.dev>
2024-05-12 19:50:46 +0100

    minor improvements

--- src/config.py
+++ src/config.py
@@ -5,5 +5,5 @@ DATA = sorted(Path("/mnt/wsl/PHYSICALDRIVE1/data/unsplash").glob("*.jpg"))
 CACHE_PATH = Path("/mnt/wsl/PHYSICALDRIVE1/data/cache2")
 CACHE_PATH.mkdir(exist_ok=True, parents=True)
 
-MODELS_PATH = Path("models")
+MODELS_PATH = Path("/home/andras/projects/bipolaroid/models")
 MODELS_PATH.mkdir(exist_ok=True, parents=True)
--- src/editor/utils/__init__.py
+++ src/editor/utils/__init__.py
@@ -5,3 +5,4 @@ from .get_colour_lut import get_colour_lut
 from .compute_histogram import compute_histogram
 from .kldiv import kldiv
 from .generate_rotation_matrices import generate_rotation_matrices
+from .get_next_run_name import get_next_run_name
