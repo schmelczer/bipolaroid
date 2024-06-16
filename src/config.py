615dafccc811163b97bf05ebb9ec00cceb72e5f6
Andras Schmelczer <andras@schmelczer.dev>
2024-06-16 20:50:51 +0100

    Extract paths

--- src/config.py
+++ src/config.py
@@ -3,8 +3,16 @@ from pathlib import Path
 DATA = sorted(Path("/mnt/wsl/PHYSICALDRIVE1/data/unsplash").glob("*.jpg"))
 TRAIN_SIZE = 0.9
 
-CACHE_PATH = Path("/mnt/wsl/PHYSICALDRIVE1/data/cache")
-CACHE_PATH.mkdir(exist_ok=True, parents=True)
-
+CACHE_PATH = Path("/mnt/wsl/PHYSICALDRIVE1/data/cache2")
 MODELS_PATH = Path("/home/andras/projects/bipolaroid/models")
-MODELS_PATH.mkdir(exist_ok=True, parents=True)
+LOGS_PATH = Path("/home/andras/projects/bipolaroid/logs")
+RUNS_PATH = Path("/home/andras/projects/bipolaroid/runs")
+
+
+for path in [
+    CACHE_PATH,
+    MODELS_PATH,
+    LOGS_PATH,
+    RUNS_PATH
+]:
+    path.mkdir(exist_ok=True, parents=True)
