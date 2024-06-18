e50087be551869249464c3a4248230983d3958ab
Andras Schmelczer <andras@schmelczer.dev>
2024-06-18 23:03:58 +0100

    Move data into config

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 
 DATA = sorted(Path("/mnt/wsl/PHYSICALDRIVE1/data/unsplash").glob("*.jpg"))
@@ -9,10 +10,18 @@ LOGS_PATH = Path("/home/andras/projects/bipolaroid/logs")
 RUNS_PATH = Path("/home/andras/projects/bipolaroid/runs")
 
 
-for path in [
-    CACHE_PATH,
-    MODELS_PATH,
-    LOGS_PATH,
-    RUNS_PATH
-]:
+for path in [CACHE_PATH, MODELS_PATH, LOGS_PATH, RUNS_PATH]:
     path.mkdir(exist_ok=True, parents=True)
+
+
+length = len(DATA)
+indices = list(range(length))
+
+random.seed(42)
+random.shuffle(indices)
+
+train_indices = indices[: int(length * TRAIN_SIZE)]
+test_indices = indices[int(length * TRAIN_SIZE) :]
+
+TRAIN_DATA = [DATA[i] for i in train_indices]
+TEST_DATA = [DATA[i] for i in test_indices]
