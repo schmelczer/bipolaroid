97e8cb1fe14aac994a1411b95a6446e0b2c77073
Andras Schmelczer <andras@schmelczer.dev>
2024-06-25 08:25:20 +0100

    Change models path

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ DATA = sorted(Path("/mnt/wsl/PHYSICALDRIVE1/data/unsplash").glob("*.jpg"))
 TRAIN_SIZE = 0.9
 
 CACHE_PATH = Path("/mnt/wsl/PHYSICALDRIVE1/data/cache2")
-MODELS_PATH = Path("/home/andras/projects/bipolaroid/models")
+MODELS_PATH = Path("/home/andras/projects/bipolaroid/saved_models")
 LOGS_PATH = Path("/home/andras/projects/bipolaroid/logs")
 RUNS_PATH = Path("/home/andras/projects/bipolaroid/runs")
 
