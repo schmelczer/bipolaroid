9d2830f8ffc4aa34bb9bbc66e265595a428b8e78
Andras Schmelczer <andras@schmelczer.dev>
2024-08-26 21:47:52 +0100

    Use new API & rerender

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ DATA = sorted(Path("/mnt/wsl/PHYSICALDRIVE0p1/featured").glob("*"))
 
 TRAIN_SIZE = 0.9
 
-CACHE_PATH = Path("/mnt/wsl/PHYSICALDRIVE1/data/cache2")
+CACHE_PATH = Path("/mnt/wsl/PHYSICALDRIVE2/data/cache3")
 MODELS_PATH = Path("/home/andras/projects/bipolaroid/saved_models")
 LOGS_PATH = Path("/home/andras/projects/bipolaroid/logs")
 RUNS_PATH = Path("/home/andras/projects/bipolaroid/runs")
